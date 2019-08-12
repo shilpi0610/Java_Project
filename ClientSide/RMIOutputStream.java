7428e676b8ebb2a68e4905e8a02551ec4dc24803
shilpi0610 <54029198+shilpi0610@users.noreply.github.com>
2019-08-12 18:43:45 +0530

    All Files Added

---
+++ ClientSide/RMIOutputStream.java
@@ -0,0 +1,30 @@
+import java.rmi.*;
+import java.rmi.registry.Registry; 
+import java.rmi.registry.LocateRegistry; 
+import java.rmi.RemoteException; 
+import java.rmi.server.UnicastRemoteObject; 
+import java.io.Serializable;
+import java.io.*;
+
+public class RMIOutputStream extends OutputStream implements Serializable {
+
+    private RMIOutputStreamInterf out;
+    
+    public RMIOutputStream(RMIOutputStreamImpl out) {
+        this.out = out;
+    }
+    
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+    public void write(byte[] b, int off, int len) throws 
+            IOException {
+        out.write(b, off, len);
+    }
+    
+    public void close() throws IOException {
+        out.close();
+    }   
+
+}
